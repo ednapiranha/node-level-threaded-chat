0f49d978b3896cd8d6b6c27442f996d4af858733
Jennifer Fong <jen@ednapiranha.com>
2013-11-19 13:10:46 -0800

    initial threaded convo implementation with tests

--- README.md
+++ README.md
@@ -1,3 +1,107 @@
 # meatspace-jamon
 
 Threaded chats with LevelDB.
+
+## Installation
+
+    > npm install
+
+## Usage
+
+    var jamon = new Jamon('you@email.com');
+
+### Follow a user
+
+    jamon.follow('friend@email.com', function (err, u) {
+      if (!err) {
+        console.log(u);
+      }
+    });
+
+### Unfollow a user
+
+    jamon.unfollow('friend@email.com', function (err, u) {
+      if (!err) {
+        console.log(u);
+      }
+    });
+
+### Get all followed users
+
+    jamon.getFollowing(function (err, f) {
+      if (!err) {
+        console.log(f);
+      }
+    });
+
+### Verify follower exists
+
+    jamon.isFollowing('friend@email.com', function (err, u) {
+      if (!err) {
+        console.log(u);
+      }
+    });
+
+### Add a chat message
+
+    var chat = {
+      media: 'http://someimage.jpg',
+      recipients: ['user1', 'user2']
+    };
+
+    chat.recipients.forEach(function (user) {
+      jamon.addChat(user, 'hola!', chat, function (err, c) {
+        if (!err) {
+          console.log(c);
+        }
+      });
+    });
+
+### Get all chats
+
+    jamon.getChats('you@email.com', <key>, <reverse>, function (err, c) {
+      if (!err) {
+        console.log(c);
+      }
+    });
+
+`key` is an optional point in which you want to start a chat stream from - set to false if you want it to default to the beginning.
+
+`reverse` is an optional boolean to reverse the chat history from latest -> earliest. Defaults at earliest -> latest.
+
+### Get parent of threaded chat
+
+    jamon.getParent('you@email.com', <key>, function (err, c) {
+      if (!err) {
+        console.log(c);
+      }
+    });
+
+## Block a user
+
+    jamon.blockUser('user@email.com', function (err, c) {
+      if (!err) {
+        console.log(c);
+      }
+    });
+
+## Unblock a user
+
+    jamon.unblockUser('user@email.com', function (err, c) {
+      if (!err) {
+        console.log(c);
+      }
+    });
+
+## Get a list of blocked users
+
+    jamon.getBlockedUsers(function (err, c) {
+      if (!err) {
+        console.log(c);
+      }
+    });
+
+
+## Tests
+
+    > make test
--- test/test.main.js
+++ test/test.main.js
@@ -3,4 +3,196 @@
 process.env.NODE_ENV = 'test';
 
 var should = require('should');
-var nock = require('nock');
+var child = require('child_process');
+var Jamon = require('../main');
+
+var chatKey;
+
+var p = new Jamon('sender@email.com', {
+  db: './test/db',
+  frequency: 1
+});
+
+describe('parallax', function () {
+  after(function () {
+    child.exec('rm -rf ./test/db');
+  });
+
+  describe('.follow',  function () {
+    it('should not follow', function (done) {
+      p.follow(' ', function (err, u) {
+        should.exist(err);
+        err.toString().should.equal('Error: Invalid user id');
+        done();
+      });
+    });
+
+    it('should follow', function (done) {
+      p.follow('receiver@email.com', function (err, u) {
+        should.exist(u);
+        u.should.equal('receiver@email.com');
+        done();
+      });
+    });
+  });
+
+  describe('.addChat', function () {
+    it('should not add a new chat to recipients', function (done) {
+      var recipients = ['receiver@email.com'];
+
+      p.addChat('receiver@email.com', 'test message', {
+        media: 'http://someimage.jpg',
+        recipients: recipients
+      }, function (err, c) {
+        should.exist(err);
+        err.toString().should.equal('Error: must be a gif or png');
+        done();
+      });
+    });
+
+    it('should add a new chat to recipients', function (done) {
+      var recipients = ['receiver@email.com'];
+
+      p.addChat('receiver@email.com', 'test message', {
+        media: 'data:image/gif;base64',
+        recipients: recipients
+      }, function (err, c) {
+        console.log('chat message: ', c);
+        should.exist(c);
+        chatKey = c.senderKey;
+        c.message.should.eql('test message');
+        c.recipients.should.eql(recipients);
+        done();
+      });
+    });
+  });
+
+  describe('.getFollowing', function () {
+    it('should get following', function (done) {
+      p.getFollowing(function (err, f) {
+        f.followed.length.should.equal(1);
+        done();
+      });
+    });
+  });
+
+  describe('.isFollowing', function () {
+    it('should verify that user is being followed', function (done) {
+      p.isFollowing('receiver@email.com', function (err, u) {
+        should.exist(u);
+        done();
+      });
+    });
+
+    it('should verify that user is not being followed', function (done) {
+      p.isFollowing('receiver2@email.com', function (err, u) {
+        should.not.exist(u);
+        done();
+      });
+    });
+  });
+
+  describe('.getChats', function () {
+    it('should get chats', function (done) {
+      p.getChats(false, false, function (err, c) {
+        should.exist(c);
+        c.chats.length.should.equal(1);
+        done();
+      });
+    });
+
+    it('should get chats in reverse', function (done) {
+      p.getChats(false, true, function (err, c) {
+        c.chats.length.should.equal(1);
+        done();
+      });
+    });
+  });
+
+  describe('.removeChat', function () {
+    it('should delete a chat', function (done) {
+      p.removeChat(chatKey, function (err, status) {
+        should.not.exist(err);
+        p.getChat(chatKey, function (err, c) {
+          should.not.exist(c);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('.blockUser', function () {
+    it('should error on blocking a user', function (done) {
+      p.blockUser('', function (err, s) {
+        should.exist(err);
+        done();
+      });
+    });
+
+    it('should block a user', function (done) {
+      p.blockUser('receiver2@email.com', function (err, s) {
+        should.exist(s);
+        done();
+      });
+    });
+
+    it('should not send a message to a blocked user', function (done) {
+      p.addChat('receiver2@email.com', 'test message', {
+        media: 'data:image/gif;base64',
+        recipients: ['receiver2@email.com']
+      }, function (err, c) {
+        should.exist(err);
+        should.not.exist(c);
+        done();
+      });
+    });
+
+    it('should not add a user who is blocked', function (done) {
+      p.follow('receiver2@email.com', function (err, u) {
+        should.exist(err);
+        should.not.exist(u);
+        done();
+      });
+    });
+  });
+
+  describe('.getBlockedUsers', function () {
+    it('should return a list of blocked users', function (done) {
+      p.getBlockedUsers(function (err, u) {
+        should.exist(u);
+        u.blocked.length.should.equal(1);
+        done();
+      });
+    });
+  });
+
+  describe('.unblockUser', function (done) {
+    it('should error on unblocking a user', function (done) {
+      p.unblockUser('', function (err, s) {
+        should.exist(err);
+        done();
+      });
+    });
+
+    it('should unblock a user', function (done) {
+      p.unblockUser('receiver2@email.com', function (err, s) {
+        should.exist(s);
+        done();
+      });
+    });
+  });
+
+  describe('.unfollow', function () {
+    it('should unfollow', function (done) {
+      p.unfollow('receiver@email.com', function (err, s) {
+        should.not.exist(err);
+        should.exist(s);
+
+        p.getFollowing(function (err, f) {
+          f.followed.length.should.equal(0);
+          done();
+        });
+      });
+    });
+  });
+});
